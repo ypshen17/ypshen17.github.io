4fa9565677382789ec2cb2b02afee0ed82673368
ypshen17 <ypshen17@users.noreply.github.com>
2025-10-03 22:07:59 +0000

    deploy: ypshen17/ypshen17-site-src@14fa260b413340cfd76e38c538cd8256c506a461

--- assets/garden/garden.js
+++ assets/garden/garden.js
@@ -4,7 +4,7 @@ let plants = [];
 let gardenShelf = [];
 
 // Load plant data
-fetch("{{ '/assets/garden/plants.json' | relative_url }}")
+fetch("{{ '/assets/garden/plants.json.html' | relative_url }}")
   .then(res => res.json())
   .then(data => {
     plants = data;
