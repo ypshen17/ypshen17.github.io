839cc7b5b8f5dc26a3a4ca81e712e9b396dce988
ypshen17 <ypshen17@users.noreply.github.com>
2025-10-16 21:02:59 +0000

    deploy: ypshen17/ypshen17-site-src@a7290406d1a8e689847be4f92f54c07d61e064d7

--- assets/field-of-forms.js
+++ assets/field-of-forms.js
@@ -30,7 +30,7 @@ let soundOn = loadPref("fieldof_sound", true); // persistent toggle
 
 // --- trail memory (keeps color) ---
 let trail = [];
-const MAX_TRAIL = 800;   // how many shapes are kept ~100 strokes × ~8 shapes
+const MAX_TRAIL = 4000;   // how many shapes are kept ~100 strokes × ~8 shapes
 const FADE_RATE = 0.99; // 0.94 = faster fade, 0.98 = slower per-frame alpha multiplier
 
 /* -------------------- preload -------------------- */
